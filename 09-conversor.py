22e46eed5892ac94ac38d24cfd886bc1b56674c4
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-26 17:46:23 -0400

    Update 09-conversor.py

--- 09-conversor.py
+++ 09-conversor.py
@@ -3,12 +3,9 @@ tempNum = float(temp)
 opcion = input("En Fahrenheit(F) o Celcius(C)?: ")
 
 if opcion.lower() == 'c':
-    
     resultado = (tempNum - 32) * 5/9
-    #resultado * 0,555
     print(resultado)
 elif opcion.lower() == 'f':
-    print("Elegiste Fahrenheit")
     resultado = tempNum * 1.8 + 32
     print(resultado)
 else:
