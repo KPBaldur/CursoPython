d1f0e76a9453e52289c5c1212a95d8ed3f58b25d
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-26 17:41:35 -0400

    Create 02-strings.py

---
+++ 02-strings.py
@@ -0,0 +1,8 @@
+texto = "Hola Mundo"
+print(texto)
+print(texto.upper())
+print(texto.lower())
+print(texto.find("Mun"))
+nuevoTexto = texto.replace("Mun", "Man")
+print(nuevoTexto)
+print("Mundo" in texto)
