53687930c7dbcffaa9faf0758e5e9d73b9a26dae
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-27 20:19:59 -0400

    Create convertidor.py

---
+++ Convertidor/convertidor.py
@@ -0,0 +1,8 @@
+menu = """
+Bienvenido al convertidor universal!
+Ingrese el convertidos que desea ingresar:
+1. Convertidor Distancia
+2. Convertidor Temperatura
+3. Convertidor volumen """
+print(menu)
+opcion = int(input("Convertidor: "))
