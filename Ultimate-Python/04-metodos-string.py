89844fd5d6ae03a2b334f5fedb3eed3c364d4b52
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-10 17:26:59 -0300

    Create 04-metodos-string.py

---
+++ Ultimate-Python/04-metodos-string.py
@@ -0,0 +1,9 @@
+animal = "  cHanchIto Feliz  "
+print(animal.upper())
+print(animal.lower())
+print(animal.capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.strip().capitalize())
+print(animal.find("I"))
+print(animal.replace("nch", "j"))
