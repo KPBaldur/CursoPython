5f350a7d3d9cd79bf4c7767b6d992124e87bc8f8
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-18 18:17:14 -0300

    Create 05-secuen-escape.py

---
+++ Ultimate-Python/tipos/05-secuen-escape.py
@@ -0,0 +1,21 @@
+#Esto es un comentario de una linea
+# \"
+# \'
+# \\
+# \n
+
+#Utilizar comillas con comillas simples
+curso = 'Ultimate "Python" '
+print(curso)
+
+#Utilizar backslash
+curso = "Ultimate \"Python\" "
+print(curso)
+
+#Mostrar un backslash
+curso = "Ultimate \\Python\\ "
+print(curso)
+
+#Saltar a una linea nuestro string
+curso = "Ultimate \nPython\\ "
+print(curso)
