77dbad79427a3193c53682091a90babf8bd6634e
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-18 18:16:56 -0300

    Delete 04-metodos-string.py

--- Ultimate-Python/04-metodos-string.py
+++
@@ -1,9 +0,0 @@
-animal = "  cHanchIto Feliz  "
-print(animal.upper())
-print(animal.lower())
-print(animal.capitalize())
-print(animal.title())
-print(animal.strip())
-print(animal.strip().capitalize())
-print(animal.find("I"))
-print(animal.replace("nch", "j"))
