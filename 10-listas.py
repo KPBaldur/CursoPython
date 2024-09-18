8523399e5d46d3dcffbc1ad47223a51ccfc1b86c
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-18 18:13:12 -0300

    Update 10-listas.py

--- 10-listas.py
+++ 10-listas.py
@@ -1,6 +1,8 @@
 lenguajes = ["html", "css", "javascript", "python", "c++"]
+print(lenguajes)
 print(lenguajes[2])
 lenguajes[2] = "Go"
 print(lenguajes)
 print(lenguajes[-1])
 print(lenguajes[0:3])
+print(lenguajes[1:])
