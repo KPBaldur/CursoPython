89969f0fb2bff4262d87e9fa28424a2c0ed44491
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-18 18:16:53 -0300

    Delete 03-format-strings.py

--- Ultimate-Python/03-format-strings.py
+++
@@ -1,9 +0,0 @@
-#1. Concatenar strings forma simple
-nombre = "Juancho"
-apellido = "Manolo"
-nombreCompleto = nombre + " " + apellido
-print(nombreCompleto)
-
-#2. Concatenar strings forma avanzada
-nombre_completo = f"{nombre} {apellido}"
-print(nombre_completo)
