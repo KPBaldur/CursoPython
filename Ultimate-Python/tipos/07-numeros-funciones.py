156f20258e2415349e679f37c13244a490c26720
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-18 18:17:19 -0300

    Create 07-numeros-funciones.py

---
+++ Ultimate-Python/tipos/07-numeros-funciones.py
@@ -0,0 +1,26 @@
+#Round: Sirve para devolver el numero entero mas cercano
+print(round(1.3))
+print(round(1.7))
+print(round(1.5))
+
+#Entregara el valor absoluto del numero
+print(abs(-77))
+print(abs(55))
+
+#Importar modulos 
+import math
+
+#Lleva el numero entero hacia arriba
+print(math.ceil(1.1))
+
+#Lleva el numero entero mas cercano hacia abajo
+print(math.floor(1.99999))
+
+#Nos regresa si el valor que le pasamos corresponde a si No es un mumero (Como 33 es un numero nos devuelve False proque ejectivamente no es un numero)
+print(math.isnan(3))
+
+#Eleva a la potencia indicada
+print(math.pow(10, 3))
+
+#Nos devuelve la raiz cuadrada del numero que nos indica
+print(math.sort(9))
