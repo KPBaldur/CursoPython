e1f220746785a39ebf8b1debaab22ba29528392c
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-18 18:17:16 -0300

    Create 06-numeros.py

---
+++ Ultimate-Python/tipos/06-numeros.py
@@ -0,0 +1,22 @@
+numero = 2 #entrero -> integer
+decimal = 1.2 # float
+imaginario = 2 + 2j # 2 + 2i
+
+#Operaciones 
+print(1 + 3)    #Suma
+print(3 - 1)    #Resta
+print(1 * 3)    #Multiplicacion
+print(1 / 3)    # Division
+print( 1 // 3)  #Division cerrada sin sobrantes o coma
+print(1 % 3)    #Modulo o sobrante de una division
+
+#Operaciones con variables
+
+#Se puede volver a modificar una variable
+numero = numero + 2
+
+#Operaciones basicas
+print(numero + 5)
+
+#Notaciones
+numero += 2
