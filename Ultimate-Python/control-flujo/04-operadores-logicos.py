9c031e360746cf19a18e7dc802ac81fc03dc24c5
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-10-28 17:59:16 -0300

    Update 04-operadores-logicos.py

--- Ultimate-Python/control-flujo/04-operadores-logicos.py
+++ Ultimate-Python/control-flujo/04-operadores-logicos.py
@@ -1,1 +1,20 @@
 # And, or, not
+
+bateria = True
+gas = True
+encendido = True
+edad = 18
+
+if gas and encendido:
+    print("El auto puede avanzar")
+
+if gas and encendido and edad > 17:
+    print("Puedes avanzar")
+
+if gas or bateria:
+    print("El auto tiene bateria y gasolina")
+else:
+    print("El auto no tiene gas o bateria")
+
+if gas and (encendido or edad > 17):
+    print("Puedes avanzar")
